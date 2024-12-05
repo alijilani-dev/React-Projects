ee23a6a47ad42caaccb4fd70f86d8931b32af460
React-Projects <aajilani.job@gmail.com>
2024-12-05 06:26:05 +0500

    Merge branch 'main'

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,11 +1,19 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+const Button = styled.button`
+background-color: black;
+color: white;
+padding: 10px;
+
+`;
 
 function App() {
 
   return (
     <>
+    <Button>This is a styled Component button</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
