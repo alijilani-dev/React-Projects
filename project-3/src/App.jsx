b1aa5a638e9e0a910734f193dea24f14357aabda
React-Projects <aajilani.job@gmail.com>
2024-12-06 11:48:08 +0500

    Push Changes

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,17 +1,11 @@
-import styled from 'styled-components';
+import StartGame from "./components/StartGame";
 
-const Button = styled.button`
-background-color: black;
-color: white;
-padding: 10px;
-
-`;
 
 function App() {
 
   return (
     <>
-    <Button>This is a styled Component button</Button>
+    <StartGame />
     </>
   );
 }
